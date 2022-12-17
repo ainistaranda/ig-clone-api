1ac2ca400ac73d1d56c37822c5ef6bed12a7e1c3
Ainis Taranda <taranda.ainis@gmail.com>
2022-12-16 22:31:54 -0500

    added like post route

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,8 @@
 import functions from "firebase-functions"
 import express from "express"
 import cors from "cors"
-import { getAllPhotos, addNewPhoto } from "./src/photos.js"
+import { getAllPhotos, addNewPhoto, likePost } from "./src/photos.js"
+
 
 const app = express()
 app.use(cors())
@@ -10,6 +11,9 @@ app.use(express.json()) // anytime we do anything other than .get
 // routes here...
 app.get('/photos', getAllPhotos) 
 app.post('/photos', addNewPhoto) 
+app.patch('/photos/:photoId', likePost)
+
+
 
 export const api = functions.https.onRequest(app)
 
